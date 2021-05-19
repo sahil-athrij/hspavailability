a8b62f4f74e4069b8b213255a1ce103624ba565c
sha-ahammed <shahahamedroze@gmail.com>
2021-05-19 23:42:47 +0530

    update marker function created

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Markers, Reviews
+from statistics import mean
 
 from django.forms.models import model_to_dict
 
@@ -47,6 +48,7 @@ def add_review(request):
 
         print(request.POST)
 
+        id = int(request.POST['id'])
         ob = Reviews.objects.create(marker_id=id)
         ob.financial_rating = int(request.POST['financial'])
         ob.avg_cost = int(request.POST['cost'])
@@ -57,3 +59,32 @@ def add_review(request):
         ob.ventilator_availability = int(request.POST['vent'])
         ob.comment = request.POST['comment']
         ob.save()
+        update_marker(id)
+
+def update_marker(id):
+    ob = Markers.objects.get(id)
+    rev = Reviews.objects.filter(marker__id=id)
+    fin = []
+    for x in rev.financial_rating:fin.append(x)
+    ob.financial_rating = mean(fin)
+    avg = []
+    for x in rev.avg_cost:avg.append(x)
+    ob.avg_cost = mean(avg)
+    covid = []
+    for x in rev.covid_rating:covid.append(x)
+    ob.covid_rating = mean(covid)
+    bed = []
+    for x in rev.beds_available:bed.append(x)
+    ob.beds_available = sum(bed)
+    care = []
+    for x in rev.care_rating:care.append(x)
+    ob.care_rating = mean(care)
+    oxy = []
+    for x in rev.oxygen_rating:oxy.append(x)
+    ob.avg_cost = mean(oxy)
+    vent = []
+    for x in rev.ventilator_availability:vent.append(x)
+    ob.ventilator_availability = sum(vent)*100/len(vent)
+
+
+

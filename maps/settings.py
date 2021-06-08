d49bc1774f83bd80c1662a96d4cb7fe96bf46ae7
Siva Jayakumar <siva.jayakumar@gmail.com>
2021-06-09 03:41:51 +0530

    Location PointField added to Markers.

--- maps/settings.py
+++ maps/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'rest_framework',
     'django_filters',
 
@@ -102,7 +103,7 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }

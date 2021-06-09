7e4dd8567a64f1a2d8e866f6f9b54bde245e3028
sathrij <sahilathrij@gmail.com>
2021-06-10 01:43:13 +0530

    fixed add_hospital_card.html

--- maps/settings.py
+++ maps/settings.py
@@ -154,3 +154,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SPATIALITE_LIBRARY_PATH = 'mod_spatialite'

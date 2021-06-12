520721a328d0e1ea5dee47cb5a44d016e14c8a5e
sathrij <sahilathrij@gmail.com>
2021-06-12 18:58:35 +0530

    fixed prod

--- location.py
+++ location.py
@@ -11,5 +11,5 @@ oblist = Markers.objects.all()
 
 for ob in oblist:
     ob.location = Point(x=ob.lng, y=ob.lat, srid=4326)
-    print(ob.id, ob.position)
+    print(ob.id, ob.location)
     ob.save(commit=True)

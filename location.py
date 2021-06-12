e9a04e4fdae33e3aba151d9b66a6c2f6c9407c90
sathrij <sahilathrij@gmail.com>
2021-06-12 18:47:39 +0530

    fixed prod

--- location.py
+++ location.py
@@ -1,6 +1,8 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','maps.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maps.settings')
 import django
+
 django.setup()
 from django.contrib.gis.geos import Point
 from home.models import Markers
@@ -8,6 +10,6 @@ from home.models import Markers
 oblist = Markers.objects.all()
 
 for ob in oblist:
-    ob.position = Point(ob.lng,ob.lat)
-    print(ob.id,ob.position)
+    ob.position = Point((ob.lng, ob.lat))
+    print(ob.id, ob.position)
     ob.save()

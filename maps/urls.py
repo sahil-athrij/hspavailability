6d466ab61ca00351d35bc121a2ab73cb944da9d3
sathrij <sahilathrij@gmail.com>
2021-06-24 01:52:12 +0530

    pushing for swagger

--- maps/urls.py
+++ maps/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -41,7 +41,7 @@ urlpatterns = [
     path('', include('v2.urls')),
     path('auth/', include('authentication.urls')),
     path('openid/', include('oidc_provider.urls', namespace='oidc_provider')),
-    path(r'swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

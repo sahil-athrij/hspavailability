01bf5f284ad45e9e5081bb612fa9d395e7124fe4
sathrij <sahilathrij@gmail.com>
2021-07-06 00:26:37 +0530

    v3 to ready i guess probably

--- maps/settings.py
+++ maps/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-tr#83t$_--71h1yzg^@w6n^w29
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['hospitals.trebuchet.one', 'needmedi.com', '127.0.0.1']
+ALLOWED_HOSTS = ['hospitals.trebuchet.one', 'needmedi.com', '127.0.0.1','13.233.106.146']
 
 # Application definition
 

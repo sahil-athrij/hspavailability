20fc0c09b0314fa331c24df6e2080ef693c1fa17
sathrij <sahilathrij@gmail.com>
2021-08-12 18:54:00 +0530

    changing some models

--- home/models.py
+++ home/models.py
@@ -123,7 +123,7 @@ class Markers(models.Model):
     type = models.CharField(choices=type, default='U', max_length=2)
     ownership = models.CharField(choices=ownership, default='U', max_length=2)
     pending_approval = models.BooleanField(default=False)
-    video_call = models.CharField(null=True, blank=True)
+    video_call = models.CharField(max_length=1000,null=True, blank=True)
 
     def __str__(self):
         return self.name

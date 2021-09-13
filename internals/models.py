d0d7feb0e274abf0c27060eb32750710489a501f
sunith vs <sunithvazhenkada@gmail.com>
2021-09-14 03:54:31 +0530

    fixing migrations

--- internals/models.py
+++ internals/models.py
@@ -52,7 +52,7 @@ class WorkingTime(models.Model):
 
 
 class HospitalWorkingTime(models.Model):
-    # working_time = models.ldCharFie(max_length=120, blank=True, null=True)
+    working_time = models.ManyToManyField(WorkingTime)
     hospital = models.ForeignKey(Markers, on_delete=models.PROTECT, blank=True, null=True)
     doctor = models.ForeignKey("Doctor", on_delete=models.PROTECT)
 
@@ -61,7 +61,7 @@ class Doctor(models.Model):
     choices = ((1,1), (2,2), (3,3), (4,4), (5,5))
     name = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=14)
-    # hospital = models.ManyToManyField(Markers, related_name='doctors', through=HospitalWorkingTime)
+    hospital = models.ManyToManyField(Markers, related_name='doctors', through=HospitalWorkingTime)
     department = models.ManyToManyField(Department, related_name='doctors')
     user = models.OneToOneField(User, related_name='doctor', on_delete=models.PROTECT, default=None, null=True, blank=True)
     about = models.TextField(blank=True,null=True, max_length=1000)

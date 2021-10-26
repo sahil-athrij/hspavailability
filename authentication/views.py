4540b5c5b3d765600f7c3a16964c7fa949f28e71
daviddeepan <davidideepan@gmail.com>
2021-10-27 00:08:35 +0530

    added custom changes for language and image in update_me

--- authentication/views.py
+++ authentication/views.py
@@ -2,12 +2,14 @@ from django.contrib.auth.models import User, Group
 from oauth2_provider.contrib.rest_framework import TokenHasScope, OAuth2Authentication
 from rest_framework import viewsets, generics, permissions
 from rest_framework.decorators import action
+from rest_framework.parsers import FileUploadParser
+from rest_framework.response import Response
 
 from rest_framework_social_oauth2.authentication import SocialAuthentication
 from .authentication import CsrfExemptSessionAuthentication
 from .permissions import IsOwner
 from .serializer import UserSerializer, GroupSerializer
-
+from home.models import Language
 
 class UserApiViewSet(viewsets.ModelViewSet):
     permission_classes = [IsOwner]
@@ -15,6 +17,7 @@ class UserApiViewSet(viewsets.ModelViewSet):
     authentication_classes = [CsrfExemptSessionAuthentication, SocialAuthentication, OAuth2Authentication]
     queryset = User.objects.all()
     http_method_names = ['get', 'post', 'put', 'patch', 'head', 'options']
+    parser_class = [FileUploadParser]
 
     def list(self, request, *args, **kwargs):
         if not request.user.is_superuser:
@@ -25,6 +28,29 @@ class UserApiViewSet(viewsets.ModelViewSet):
     def me(self, request, *args, **kwargs):
         self.queryset = self.queryset.filter(pk=request.user.pk)
         return viewsets.ModelViewSet.list(self, request, *args, **kwargs)
+
+    @action(detail=False, methods=['patch'], url_path='update_me')
+    def update_me(self, request, *args, **kwargs):
+
+
+
+        user = request.user
+        token = user.tokens
+        languages = request.data['languages']
+
+        try:
+            profile = request.FILES['image']
+            token.profile=profile
+        except:
+            pass
+
+        print(languages)
+        for ln in languages:
+            lang_obj,_ = Language.objects.get_or_create(name=ln.lower())
+            token.language.add(lang_obj.id)
+        token.save()
+        return Response(status=201)
+
 
 
 class GroupList(generics.ListAPIView):

91969f06395d11739d9507e8c9766da0b39a596c
sunith <sunithvs2002@gmail.com>
2021-11-14 00:28:30 +0530

    edited message  model

--- chats/models.py
+++ chats/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
 
 # Create your models here.
@@ -10,6 +9,6 @@ class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sender', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='receiver', on_delete=models.CASCADE)
     message = models.TextField()
-    sent_at = models.DateTimeField(_("sent at"), null=True, blank=True, auto_now_add=True)
-    read_at = models.DateTimeField(_("read at"), null=True, blank=True)
-    ip = models.GenericIPAddressField(verbose_name=_('IP'), null=True, blank=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    read_at = models.DateTimeField(null=True, blank=True)
+    ip = models.GenericIPAddressField(null=True, blank=True)

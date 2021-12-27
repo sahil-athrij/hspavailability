9c7122845ab7a61fc60dcd70c9d34d5760033ed3
Rohit T P <tprohit9@gmail.com>
2021-12-27 17:54:33 +0530

    in memory chat finished ( for real 🎊🎊 )

--- chats/consumers.py
+++ chats/consumers.py
@@ -10,47 +10,35 @@ devices = {}
 class ChatConsumer(AsyncWebsocketConsumer):
     def __init__(self):
         self.username = ''
-        self.room_name, self.room_group_name = None, None
         super().__init__()
 
     async def connect(self):
-        self.room_name = "room"
-        self.room_group_name = f'chat_room'
-
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
         await self.accept()
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+    async def disconnect(self, code):
+        websockets.pop(self.username)
+        devices.pop(self.username)
 
     async def receive(self, text_data=None, bytes_data=None):
         message = json.loads(text_data)
         msg_type = message['type']
 
         if msg_type == 'register':
-            user = message['user']
-            self.username = user.username
-            if self.username in websockets:
-                websockets[self.username].append(self)
-            else:
-                websockets[self.username] = [self]
+            self.username = message['username']
+            websockets[self.username] = [*(websockets[self.username] if self.username in websockets else []), self]
             devs = devices[self.username] if self.username in devices else []
 
             await self.send(text_data=json.dumps({
                 'type': 'registered',
                 'devices': devs,
             }))
-            for dev in set(devs) - {self.username}:
+
+            for dev in set(devices) - {message['username']}:
+                print(dev)
                 await self.send(text_data=json.dumps({
                     'type': 'devices',
                     'devices': devices[dev],
-                    'username': dev,
+                    'username': message['username'],
                 }))
 
         elif msg_type == 'bundle':
@@ -58,9 +46,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         elif msg_type == 'devices':
             devices[message["username"]] = message['devices']
+
             for user in websockets:
                 if user != message["username"]:
-                    websockets[user].send(text_data=json.dumps(message))
+                    for socket in websockets[user]:
+                        await socket.send(text_data=json.dumps(message))
 
         elif msg_type == 'getBundle':
             if message["deviceId"] in bundles:
@@ -74,5 +64,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             if message["to"] not in websockets:
                 return
 
+            print("Sending", message)
+
             for socket in websockets[message["to"]]:
-                socket.send(json.dumps(message))
+                await socket.send(json.dumps(message))

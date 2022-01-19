9f3ab51aa6a39d030c05a6141053f22c83009ec2
sunith <sunithvs2002@gmail.com>
2022-01-19 23:18:38 +0530

    msg is ok

--- chats/consumers.py
+++ chats/consumers.py
@@ -1,6 +1,9 @@
 import json
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+from .models import ChatUser
 
 websockets = {}
 bundles = {}
@@ -10,31 +13,47 @@ devices = {}
 class ChatConsumer(AsyncWebsocketConsumer):
     def __init__(self):
         self.username = ''
+
         super().__init__()
 
     async def connect(self):
         await self.accept()
 
     async def disconnect(self, code):
-        websockets.pop(self.username)
-        devices.pop(self.username)
+        print('Disconnect from', self.username, code)
+
+        if self.username:
+            try:
+                websockets.pop(self.username)
+            except KeyError:
+                print(f"{self.username} not found on websockets")
+            try:
+                devices.pop(self.username)
+            except KeyError:
+                print(f"{self.username} not found on device")
 
     async def receive(self, text_data=None, bytes_data=None):
         message = json.loads(text_data)
+        # user = self.scope['user']
+        # print(user)
+        # print(f"{self.user = }")
         msg_type = message['type']
-
         if msg_type == 'register':
-            self.username = message['username']
+
+            user = await self.get_user(message["username"])
+            self.username = user.id
             websockets[self.username] = [*(websockets[self.username] if self.username in websockets else []), self]
             devs = devices[self.username] if self.username in devices else []
+            print(f'{devices = }')
+            print(f"{message = }")
 
             await self.send(text_data=json.dumps({
                 'type': 'registered',
                 'devices': devs,
             }))
+            print("send the devs")
 
             for dev in set(devices) - {message['username']}:
-                print(dev)
                 await self.send(text_data=json.dumps({
                     'type': 'devices',
                     'devices': devices[dev],
@@ -42,9 +61,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 }))
 
         elif msg_type == 'bundle':
+            # Bundle.objects.create(data=message['bundle'])
             bundles[message["deviceId"]] = message['bundle']
 
         elif msg_type == 'devices':
+            # Devices.objects.create(data=message['devices'])
             devices[message["username"]] = message['devices']
 
             for user in websockets:
@@ -61,10 +82,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 }))
 
         elif msg_type == 'message':
+            print(f'{message = }')
             if message["to"] not in websockets:
-                return
-
-            print("Sending", message)
-
+                return print(message["to"], "Not found")
+            ids = []
+            for i in message['encrypted']['header']['keys']:
+                if i['rid'] != message['encrypted']['header']['sid']:
+                    ids.append(i)
+            # message['encrypted']['header']['keys'] = ids
+            print(f'{message = }')
             for socket in websockets[message["to"]]:
                 await socket.send(json.dumps(message))
+
+        else:
+            print("Unknown message type", message)
+
+    @database_sync_to_async
+    def get_user(self, uid):
+        return ChatUser.objects.get(id=uid)
--- chats/models.py
+++ chats/models.py
@@ -1,1 +1,44 @@
+import random
+import string
 
+from django.contrib.auth.models import User
+from django.contrib.gis.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class Message(models.Model):
+    data = models.JSONField(blank=True, null=True)
+    to_user = models.ForeignKey('ChatUser', related_name='messages', on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)
+
+
+class Bundle(models.Model):
+    data = models.JSONField(blank=True, null=True)
+    user = models.ForeignKey('ChatUser', on_delete=models.CASCADE, related_name='bundle')
+
+
+class Devices(models.Model):
+    data = models.JSONField(blank=True, null=True)
+
+
+def user_id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def create_user_id():
+    not_unique = True
+    unique_id = user_id_generator()
+    while not_unique:
+        unique_id = user_id_generator()
+        if not ChatUser.objects.filter(id=unique_id):
+            not_unique = False
+    return str(unique_id)
+
+
+class ChatUser(models.Model):
+    id = models.CharField(default=create_user_id, primary_key=True, max_length=10)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    device_ids = ArrayField(models.PositiveIntegerField(), blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.id} {self.user.username}"

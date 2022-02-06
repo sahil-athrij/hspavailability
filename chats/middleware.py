6f976941350270006cee2d85e756dd3b852e708d
Rohit T P <tprohit9@gmail.com>
2022-02-06 16:48:16 +0530

    middleware something

--- chats/middleware.py
+++ chats/middleware.py
@@ -12,7 +12,6 @@ logger = getLogger('chat')
 def get_user(token_key):
     try:
         token = Token.objects.get(key=token_key)
-        print(f"obj {token = }")
         return token.user
     except Token.DoesNotExist:
         return AnonymousUser()
@@ -23,13 +22,10 @@ class TokenAuthMiddleware(BaseMiddleware):
         super().__init__(inner)
 
     async def __call__(self, scope, receive, send):
-        logger.info("from middleware")
         try:
-
             # token_key = None
             token_key = (dict((x.split('=') for x in scope['query_string'].decode().split("&")))).get('token', None)
-            print(f"{token_key = }")
+            scope['user'] = get_user(token_key)
         except ValueError:
-            token_key = None
-        scope['user'] = AnonymousUser()  # if token_key is None else await get_user(token_key)
+            scope['user'] = AnonymousUser()  # if token_key is None else await get_user(token_key)
         return await super().__call__(scope, receive, send)
